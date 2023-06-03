51898b827982b6d58c0c26c20c6676d2f8fcf356
Gamers00 <133620517+Gamers00@users.noreply.github.com>
2023-06-03 20:30:50 -0300

    Error

---
+++ script error.lua
@@ -0,0 +1,5 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Sorry script not working!";
+    Text = "Discord to news updates https://discord.gg/wynhjfZJ";
+    Button1 = "Ok Thank you";
+})
